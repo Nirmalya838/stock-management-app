8a17e1dd2f0e94106570ea95ae990b54f1ee39a1
Nirmalya838 <122223948+Nirmalya838@users.noreply.github.com>
2023-05-16 15:47:23 +0530

    New modified Stock Management app

--- main.js
+++ main.js
@@ -1,133 +1,126 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const candyInfo = document.getElementById("candy-info");
-  axios.get('https://crudcrud.com/api/d4b59b7348924d70ad183103040e5180/data')
-    .then((response) => {
-      const candies = response.data;
-      candies.forEach((candy, index) => {
-        const listItem = document.createElement("li");
-        const text = document.createTextNode(candy.Candyname + " " + candy.Description + " " + candy.Price + " " + candy.Quantity);
-        listItem.appendChild(text);
-        let cand = candy._id
+function displayUserDetails() {
+    let info = document.getElementById('info');
+    axios.get('https://crudcrud.com/api/f68dc33c172549049bb411945d242b5d/data')
+      .then((response) => {
+        console.log(response);
+        for (let i in response.data) {
+          let n = document.createElement('li');
+          let text = document.createTextNode(response.data[i].candyName + " - " + response.data[i].Quantity);
+          n.appendChild(text);
+  
+          let buy = document.createElement("button");
+          buy.textContent = "buy one";
+          buy.setAttribute("id", "bt" + i);
+          n.appendChild(buy);
 
-        const buy1 = document.createElement("button");
-        buy1.textContent = "BUY 1";
-        buy1.setAttribute("id", "btn" + index);
-        buy1.addEventListener("click", () => {
-          axios.delete(`https://crudcrud.com/api/d4b59b7348924d70ad183103040e5180/data/${candy._id}`)
-            .then((response) => {
-              console.log(response);
-              // listItem.remove();
-              location.reload();
-            })
-            .catch((error) => {
-              console.error(error);
-            });
-        let q = parseInt(listItem.textContent.split(" ")[3])-1
-        let obj = {
-          "Candyname" : listItem.textContent.split(" ")[0],
-          "Description" : listItem.textContent.split(" ")[1],
-          "Price" : listItem.textContent.split(" ")[2],
-          "Quantity" : q
-        }
-        axios.post("https://crudcrud.com/api/d4b59b7348924d70ad183103040e5180/data", obj)
-            .then((response) => {
-              console.log(response);
-              location.reload();
-            })
-            .catch((error) => {
-              console.error(error);
-            });
-      });
-            listItem.appendChild(buy1);
- 
-            const buy2 = document.createElement("button");
-            buy2.textContent = "BUY 2";
-            buy2.setAttribute("id", "btn1" + index);
-            buy2.addEventListener("click", () => {
-              axios.delete(`https://crudcrud.com/api/d4b59b7348924d70ad183103040e5180/data/${candy._id}`)
-                .then((response) => {
-                  console.log(response);
-                  location.reload();
-                })
-                .catch((error) => {
-                  console.error(error);
-                });
-            let q = parseInt(listItem.textContent.split(" ")[3])-2
+          buy.addEventListener('click', () => {
             let obj = {
-              "Candyname" : listItem.textContent.split(" ")[0],
-              "Description" : listItem.textContent.split(" ")[1],
-              "Price" : listItem.textContent.split(" ")[2],
-              "Quantity" : q
-            }
-            axios.post("https://crudcrud.com/api/d4b59b7348924d70ad183103040e5180/data", obj)
-                .then((response) => {
-                  console.log(response);
-                  location.reload();
-                })
-                .catch((error) => {
-                  console.error(error);
-                });
-          });
-                listItem.appendChild(buy2);
-
-                const buy3 = document.createElement("button");
-                buy3.textContent = "BUY 3";
-                buy3.setAttribute("id", "btn2" + index);
-                buy3.addEventListener("click", () => {
-                  axios.delete(`https://crudcrud.com/api/d4b59b7348924d70ad183103040e5180/data/${candy._id}`)
-                    .then((response) => {
-                      console.log(response);
-                      location.reload();
-                    })
-                    .catch((error) => {
-                      console.error(error);
-                    });
-                let q = parseInt(listItem.textContent.split(" ")[3])-3
-                let obj = {
-                  "Candyname" : listItem.textContent.split(" ")[0],
-                  "Description" : listItem.textContent.split(" ")[1],
-                  "Price" : listItem.textContent.split(" ")[2],
-                  "Quantity" : q
-                }
-                axios.post("https://crudcrud.com/api/d4b59b7348924d70ad183103040e5180/data", obj)
-                    .then((response) => {
-                      console.log(response);
-                      location.reload();
-                    })
-                    .catch((error) => {
-                      console.error(error);
-                    });
+              candyName: response.data[i].candyName,
+              Quantity: response.data[i].Quantity - 1
+            };
+            axios.put(`https://crudcrud.com/api/f68dc33c172549049bb411945d242b5d/data/${response.data[i]._id}`, obj)
+              .then((response) => {
+                console.log(response);
+                location.reload();
+              })
+              .catch((error) => {
+                console.error(error);
               });
-                    listItem.appendChild(buy3);
-                 candyInfo.appendChild(listItem);
-      });
-    })
-    .catch((error) => {
-      document.body.innerHTML = "<h3>Something went wrong</h3>";
-      console.error(error);
-    });
+          });
 
-  });
-let sub = document.getElementById("submit");
-sub.addEventListener("click", () => {
-  let c = document.getElementById("candyname").value;
-  let d = document.getElementById("description").value;
-  let e = document.getElementById("price").value;
-  let f = document.getElementById("quantity").value;
-    let object = {
-    "Candyname" : c,
-    "Description" : d,
-    "Price" : e,
-    "Quantity" : f
+          let buyy = document.createElement("button");
+          buyy.textContent = "buy two";
+          buyy.setAttribute("id", "btt" + i);
+          n.appendChild(buyy);
+  
+          buyy.addEventListener('click', () => {
+            let obj = {
+              candyName: response.data[i].candyName,
+              Quantity: response.data[i].Quantity - 2
+            };
+            axios.put(`https://crudcrud.com/api/f68dc33c172549049bb411945d242b5d/data/${response.data[i]._id}`, obj)
+              .then((response) => {
+                console.log(response);
+                location.reload();
+              })
+              .catch((error) => {
+                console.error(error);
+              });
+          });
+
+          let buyyy = document.createElement("button");
+          buyyy.textContent = "buy three";
+          buyyy.setAttribute("id", "bttt" + i);
+          n.appendChild(buyyy);
+
+          buyyy.addEventListener('click', () => {
+            let obj = {
+              candyName: response.data[i].candyName,
+              Quantity: response.data[i].Quantity - 3
+            };
+            axios.put(`https://crudcrud.com/api/f68dc33c172549049bb411945d242b5d/data/${response.data[i]._id}`, obj)
+              .then((response) => {
+                console.log(response);
+                location.reload();
+              })
+              .catch((error) => {
+                console.error(error);
+              });
+          });
+
+          let bur = document.createElement("button");
+          bur.textContent = "buy";
+          bur.setAttribute("id", "br" + i);
+          let box = document.createElement("input");
+          box.type = "number";
+          n.appendChild(bur);
+          n.appendChild(box);
+
+          bur.addEventListener('click', () => {
+            let obj = {
+              candyName: response.data[i].candyName,
+              Quantity: response.data[i].Quantity - box.value
+            };
+            axios.put(`https://crudcrud.com/api/f68dc33c172549049bb411945d242b5d/data/${response.data[i]._id}`, obj)
+              .then((response) => {
+                console.log(response);
+                location.reload();
+              })
+              .catch((error) => {
+                console.error(error);
+              });
+          });
+
+
+          info.appendChild(n);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
-  axios.post('https://crudcrud.com/api/d4b59b7348924d70ad183103040e5180/data', object)
-       .then((response)=>{
+  
+  window.addEventListener('load', displayUserDetails);
+  
+  let btn = document.getElementById('submit');
+  btn.addEventListener('click', () => {
+    let cname = document.getElementById('toffee').value;
+    let qty = document.getElementById('quantity').value;
+    let price = document.getElementById('price').value;
+  
+    let object = {
+      candyName: cname,
+      Quantity: qty,
+      Price : price
+    };
+  
+    axios.post('https://crudcrud.com/api/f68dc33c172549049bb411945d242b5d/data', object)
+      .then((response) => {
         console.log(response);
         location.reload();
-
-       })
-       .catch((error)=>{
+      })
+      .catch((error) => {
         console.error(error);
-       })
-       
-});
+      });
+  });
+  
